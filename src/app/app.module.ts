2a58b9650f18ba3e5eb29f9459dd3bb8691ccacd
hugoisidore <hugo.isidore@oclock.school>
2025-01-03 14:53:59 +0100

    scroll nav to section with angular done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { RouterModule, Routes } from '@angular/router'; 
+import { RouterModule, Routes, ExtraOptions } from '@angular/router'; 
 
 import { AppComponent } from './app.component';
 import { ReservationFormComponent } from './components/reservation-form/reservation-form.component';
@@ -12,19 +12,27 @@ import { ClientService } from './services/client.service';
 import { TableService } from './services/table.service';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'reservation', pathMatch: 'full' },
+  { path: '', component: AppComponent },
   { path: 'reservation', component: ReservationFormComponent }
 ];
 
+const routerOptions: ExtraOptions = {
+  scrollPositionRestoration: 'enabled', // Restaure la position de défilement
+  anchorScrolling: 'enabled',          // Active le défilement vers les ancrages
+  scrollOffset: [0, 50],               // Décale légèrement pour laisser de l'espace au menu
+};
+
 @NgModule({
+  declarations: [ 
+    AppComponent,
+    ReservationFormComponent,
+  ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpClientModule,
     BrowserAnimationsModule,
-    RouterModule.forRoot(routes),
-    AppComponent,
-    ReservationFormComponent,
+    RouterModule.forRoot(routes, routerOptions),
   ],
   providers: [ReservationService, ClientService, TableService],
   bootstrap: [AppComponent]
