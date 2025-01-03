20a12326079f45b52c8ecba84d388d624ce76ee4
hugoisidore <hugo.isidore@oclock.school>
2025-01-03 15:37:16 +0100

    config menu updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,14 @@ import { Router, ActivatedRoute } from '@angular/router';
 import { ViewportScroller } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { ReservationFormComponent } from './components/reservation-form/reservation-form.component';
+import { MenuContentComponent } from './components/menu-content/menu-content.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   standalone: true,
-  imports: [RouterModule, ReservationFormComponent],
+  imports: [RouterModule, ReservationFormComponent, MenuContentComponent],
 })
 export class AppComponent {
   constructor(
